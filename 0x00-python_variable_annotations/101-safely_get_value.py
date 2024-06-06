d9cfb1e2cf71b318c886f6025ed6722a887ffabe
ubonisrael <akpanudohubonisrael@gmail.com>
2024-06-06 14:34:28 +0100

    0x00 task 12: fixed pycodestyle errors

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,8 +4,12 @@ from typing import Mapping, Union, Any, TypeVar
 from types import NoneType
 
 T = TypeVar("T")
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, NoneType] = None) -> Union[Any, T]:
-    """safetly returns a value"""
+Tvar = Union[T, NoneType]
+Dvar = Union[Any, T]
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Tvar = None) -> Dvar:
+    """safely returns a value"""
     if key in dct:
         return dct[key]
     else:
