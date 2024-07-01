613838341bd0ba1f4487c05f48bab25fd1fe893c
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-01 20:40:24 +0100

    0x03 task 1 initial attempt

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,6 +15,18 @@ class TestAcessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Union[Dict, int]) -> None:
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
+                               expected: Union[Dict, int]) -> None:
         """tests the output of the access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+        ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping, path: Sequence,
+                                         expected: KeyError) -> None:
+        """tests invalid output for the access_nested_map function"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
